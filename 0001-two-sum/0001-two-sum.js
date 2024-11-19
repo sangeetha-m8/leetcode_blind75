0b39d6ddd060d3609d5f0ac20f9cc355bce68dea
Shubham <shubhamsarda99@yahoo.com>
2024-11-19 16:08:01 +0530

    DSA / Leetcode / JavaScript

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+    let hashmap = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if (complement in hashmap) {
+            return [hashmap[complement], i];
+        }
+
+        hashmap[nums[i]] = i;
+    }
+};
